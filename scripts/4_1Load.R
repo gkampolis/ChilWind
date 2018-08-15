ed6759c6c3b63eb882c1cfb920dc529da580cdd7
George Kampolis <gkampolis@outlook.com>
2018-08-15 17:22:17 +0100

    Added ETS models, beginning ARIMA. #WIP

--- scripts/4_1Load.R
+++ scripts/4_1Load.R
@@ -19,6 +19,14 @@ windTrainTS <- msts(windTrain$windSpeed, freq, ts.frequency = freq[1])
 
 windTestTS <- msts(windTest$windSpeed, freq, ts.frequency = freq[1])
 
+# Shift the Test data in time to accomodate plotting. Start & End points
+# are the same as the forecast objects. This does not truncate the series.
+windTestTS <- ts(data = as.vector(windTestTS),
+                frequency = 24,
+                start = c(746, 1),
+                end = c(762, 24)
+)
+
 # Set forecast horizon h
 horizon <- length(windTestTS)
 
--- scripts/4_3ClassicalModels.R
+++ scripts/4_3ClassicalModels.R
@@ -11,3 +11,25 @@
 # Variables for the predictions are forName, for "forecasts".
 
 # ######################## ETS models ##########################################
+
+fitETS <- ets(windTrainTS, lambda = NULL)
+
+forETS <- forecast(fitETS, h = horizon, lambda = NULL)
+
+fitETSBoxCoxAuto <- ets(windTrainTS, lambda = "auto")
+
+forETSBoxCoxAuto <- forecast(fitETSBoxCoxAuto, h = horizon, biasadj = TRUE)
+
+fitETSBoxCox <- ets(windTrainTS + 1, lambda = 0, biasadj = TRUE)
+
+forETSBoxCox <- forecast(fitETSBoxCox, h = horizon, biasadj = TRUE)
+
+# Back-adjust the addition of unity to box-cox
+forETSBoxCox$mean <- forETSBoxCox$mean - 1
+forETSBoxCox$upper <- forETSBoxCox$upper - 1
+forETSBoxCox$lower <- forETSBoxCox$lower - 1
+
+# ######################## ARIMA models ########################################
+
+## Notify that script's end has been reached ##
+if (require(beepr)) {beepr::beep(1)}
